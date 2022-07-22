22659743c323c7fb354859290cd4b7e2853887e9
kanaru-ssk <2019january.0131@gmail.com>
2022-07-22 11:43:28 +0900

    質問が見つからない時の表示変更

--- src/pages/question/[id].tsx
+++ src/pages/question/[id].tsx
@@ -73,79 +73,88 @@ const Question = ({ questionSsr }: Props) => {
     navigator.clipboard.writeText(location.href);
     setIsCopy(true);
   };
-  if (isNotFound) {
-    return <NotFound />;
-  } else {
-    return (
-      <>
-        <Head>
-          <link
-            rel="canonical"
-            href={process.env.NEXT_PUBLIC_URL + "/question/" + id}
-          />
 
-          <meta property="og:url" content={process.env.NEXT_PUBLIC_URL} />
-          <meta property="og:type" content="website" />
-          <meta
-            property="og:image"
-            content={process.env.NEXT_PUBLIC_URL + "/img/ogp.png"}
-          />
-          <meta
-            property="og:title"
-            content={
-              question?.question
-                ? "回答箱 | 「" + question?.question + "」"
-                : "回答箱"
-            }
-          />
-          <meta
-            property="og:description"
-            content={
-              "質問を作成して匿名で回答を募集しよう!" + question?.question
-                ? " 質問 : 「" + question?.question + "」"
-                : ""
-            }
-          />
-          <meta name="twitter:card" content="summary_large_image" />
+  return (
+    <>
+      <Head>
+        <link
+          rel="canonical"
+          href={process.env.NEXT_PUBLIC_URL + "/question/" + id}
+        />
 
-          <title>
-            {question?.question
+        <meta property="og:url" content={process.env.NEXT_PUBLIC_URL} />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:image"
+          content={process.env.NEXT_PUBLIC_URL + "/img/ogp.png"}
+        />
+        <meta
+          property="og:title"
+          content={
+            question?.question
               ? "回答箱 | 「" + question?.question + "」"
-              : "回答箱"}
-          </title>
-          <meta
-            name="description"
-            content={
-              "質問を作成して匿名で回答を募集しよう!" + question?.question
-                ? " 質問 : 「" + question?.question + "」"
-                : ""
-            }
-          />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
+              : "回答箱"
+          }
+        />
+        <meta
+          property="og:description"
+          content={
+            "質問を作成して匿名で回答を募集しよう!" + question?.question
+              ? " 質問 : 「" + question?.question + "」"
+              : ""
+          }
+        />
+        <meta name="twitter:card" content="summary_large_image" />
 
-        <main className="px-4">
-          <section>
-            <h2>質問</h2>
-            <div className="w-full rounded-3xl border-2 border-main-color bg-light-gray py-12 text-center">
-              {question ? question.question : <Loading />}
-            </div>
+        <title>
+          {question?.question
+            ? "回答箱 | 「" + question?.question + "」"
+            : "回答箱"}
+        </title>
+        <meta
+          name="description"
+          content={
+            "質問を作成して匿名で回答を募集しよう!" + question?.question
+              ? " 質問 : 「" + question?.question + "」"
+              : ""
+          }
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-            <h3 className="text-center">質問のリンクを共有しよう！</h3>
-            <div className="flex justify-center gap-4 ">
-              <Button
-                text={isCopy ? "copied !" : "リンクをコピー"}
-                color="gray"
-                onClick={copyLink}
-              />
-              <Button
-                text="Tweet"
-                color="twitter-color"
-                onClick={shareOnTwitter}
-              />
-            </div>
-          </section>
+      <main className="px-4">
+        <section>
+          <h2>質問</h2>
+          <div className="w-full rounded-3xl border-2 border-main-color bg-light-gray py-12 text-center">
+            {isNotFound ? (
+              <span className="text-gray">質問が見つかりませんでした。</span>
+            ) : question ? (
+              question.question
+            ) : (
+              <Loading />
+            )}
+          </div>
 
+          {!isNotFound && (
+            <>
+              <h3 className="text-center">質問のリンクを共有しよう！</h3>
+              <div className="flex justify-center gap-4 ">
+                <Button
+                  text={isCopy ? "copied !" : "リンクをコピー"}
+                  color="gray"
+                  onClick={copyLink}
+                />
+                <Button
+                  text="Tweet"
+                  color="twitter-color"
+                  onClick={shareOnTwitter}
+                />
+              </div>
+            </>
+          )}
+        </section>
+
+        {!isNotFound && (
           <section>
             <h2>匿名で回答する</h2>
             <form onSubmit={onSubmitHundler}>
@@ -158,26 +167,28 @@ const Question = ({ questionSsr }: Props) => {
               <SubmitButton text="回答する" />
             </form>
           </section>
+        )}
 
+        {!isNotFound && (
           <section>
             <h2>回答一覧</h2>
             <Answers />
           </section>
+        )}
 
-          <section className="text-center">
-            <p className="pb-4">質問を作成して匿名の回答を募集しましょう!</p>
-            <Button
-              text="質問を作成する"
-              color="main-color"
-              onClick={() => {
-                router.push("/");
-              }}
-            />
-          </section>
-        </main>
-      </>
-    );
-  }
+        <section className="text-center">
+          <p className="pb-4">質問を作成して匿名の回答を募集しましょう!</p>
+          <Button
+            text="質問を作成する"
+            color="main-color"
+            onClick={() => {
+              router.push("/");
+            }}
+          />
+        </section>
+      </main>
+    </>
+  );
 };
 
 export default Question;
