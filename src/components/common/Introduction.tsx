9949ab655076183f4494de7fc7cfe6a3d35cc305
kanaru-ssk <2019january.0131@gmail.com>
2022-07-24 17:59:15 +0900

    h2をboldに変更

--- src/components/common/Introduction.tsx
+++ src/components/common/Introduction.tsx
@@ -2,7 +2,7 @@ const Introduction = () => {
   return (
     <div>
       <div className="my-12 text-center"></div>
-      <h1>回答箱とは?</h1>
+      <h2>回答箱とは?</h2>
       <p>回答箱は、質問を作成して、匿名の回答を募集できるwebサービスです。</p>
       <p>ログイン不要でリンクを共有するだけ！</p>
       <h2>使い方</h2>
