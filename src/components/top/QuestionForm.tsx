480b06dc666b9b40ecaaf81e1d446b3bce95e9e3
kanaru-ssk <2019january.0131@gmail.com>
2022-07-24 21:32:18 +0900

    質問未入力時、作成ボタンを非アクティブ化

--- src/components/top/QuestionForm.tsx
+++ src/components/top/QuestionForm.tsx
@@ -11,14 +11,28 @@ const QuestionForm = () => {
   const user = useAuth();
   const [newQuestion, setNewQuestion] = useState<string>("");
   const [isCreating, setIsCreating] = useState<boolean>(false);
+  const [isReady, setIsReady] = useState<boolean>(false);
+
+  const onChangeHundler = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    if (typeof e.target.value === "string") {
+      setNewQuestion(e.target.value);
+      if (e.target.value.length > 0) {
+        setIsReady(true);
+      } else {
+        setIsReady(false);
+      }
+    }
+  };
 
   const onSubmitHundler = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    setIsCreating(true);
-    if (user && newQuestion) {
-      createQuestion(newQuestion).then((result) => {
-        if (result) router.push("question/" + result);
-      });
+    if (isReady) {
+      setIsCreating(true);
+      if (user && newQuestion) {
+        createQuestion(newQuestion).then((result) => {
+          if (result) router.push("question/" + result);
+        });
+      }
     }
   };
 
@@ -35,7 +49,7 @@ const QuestionForm = () => {
           name="answer"
           placeholder="質問を入力して下さい。"
           value={newQuestion}
-          onChange={(e) => setNewQuestion(e.target.value)}
+          onChange={onChangeHundler}
           className="absolute top-0 h-full w-full resize-none overflow-visible rounded-3xl border-2 border-gray px-5 py-12 text-center leading-5"
         ></textarea>
       </div>
@@ -47,7 +61,11 @@ const QuestionForm = () => {
         ) : (
           <button
             type="submit"
-            className="w-40 rounded-full bg-main-color py-3 font-bold text-white"
+            className={
+              (isReady
+                ? "bg-main-color font-bold text-white"
+                : "bg-light-gray text-dark-gray") + " w-40 rounded-full py-3"
+            }
           >
             質問作成
           </button>
